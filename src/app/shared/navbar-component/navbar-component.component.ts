2c50bc1752ffc565f869311af4965b3d080e6d58
Florin <stoflorin22@gmail.com>
2023-12-25 10:33:14 +0200

    remove navbar from unauthorized

--- src/app/shared/navbar-component/navbar-component.component.ts
+++ src/app/shared/navbar-component/navbar-component.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-component',
   templateUrl: './navbar-component.component.html',
   styleUrl: './navbar-component.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private router: Router) {
+    this.DetermineNavbarUsage();
+  }
+  useNavbar: boolean = true;
+  public DetermineNavbarUsage() {
+    this.router.events.subscribe((event: any) => {
+      if (
+        event instanceof NavigationEnd &&
+        this.router.url == '/unauthorized'
+      ) {
+        this.useNavbar = false;
+      }
+    });
+  }
+}
