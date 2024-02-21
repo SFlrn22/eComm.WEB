565dbbb33befc9096b007eea31c7be595426a31c
Florin <stoflorin22@gmail.com>
2024-02-21 20:26:35 +0200

    products-page options

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
-  styleUrl: './products-page.component.scss'
+  styleUrl: './products-page.component.scss',
 })
 export class ProductsPageComponent {
+  isRecording: boolean = false;
 
+  startRecording() {
+    this.isRecording = true;
+    console.log('start');
+  }
+
+  stopRecording() {
+    this.isRecording = false;
+    console.log('stop');
+  }
 }
--- src/app/pages/products-page/products-page.module.ts
+++ src/app/pages/products-page/products-page.module.ts
@@ -3,15 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsPageRoutingModule } from './products-page-routing.module';
 import { ProductsPageComponent } from './products-page.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    ProductsPageComponent
-  ],
+  declarations: [ProductsPageComponent],
   imports: [
     CommonModule,
-    ProductsPageRoutingModule
-  ]
+    ProductsPageRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+  ],
 })
-export class ProductsPageModule { }
+export class ProductsPageModule {}
